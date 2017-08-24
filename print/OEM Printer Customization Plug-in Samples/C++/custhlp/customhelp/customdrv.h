3965a237da143d06f0a24e042cef0970b799ea19
Mike Fenelon <mfenelon@ntdev.microsoft.com>
2017-08-24 10:10:48 -0700

    Initial add of V3 Samples

---
+++ print/OEM Printer Customization Plug-in Samples/C++/custhlp/customhelp/customdrv.h
@@ -0,0 +1,11 @@
+//
+//
+//
+//
+//////////////////////////////////////////////////////////////////
+
+#define CUSDRV_HELPTOPIC_1 = 15001
+#define CUSDRV_HELPTOPIC_2 = 15002
+#define CUSDRV_HELPTOPIC_3 = 15003
+#define CUSDRV_HELPTOPIC_4 = 15004
+
