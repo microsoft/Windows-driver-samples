3187b6146cbb53c18cab7de3de4f27f4e6a43297
Vidush Vishwanath <vivis@microsoft.com>
2019-08-05 11:31:31 -0700

    Fix Comments for DriverEntry function

--- usb/kmdf_enumswitches/sys/driver.c
+++ usb/kmdf_enumswitches/sys/driver.c
@@ -107,7 +107,7 @@ Return Value:
                        "OSRUSBFX2 Driver Sample - Driver Framework Edition.\n");
 
     //
-    // Initiialize driver config to control the attributes that
+    // Initialize driver config to control the attributes that
     // are global to the driver. Note that framework by default
     // provides a driver unload routine. If you create any resources
     // in the DriverEntry and want to be cleaned in driver unload,
