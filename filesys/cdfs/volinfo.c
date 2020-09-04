b77c7130832a6450222502e804ce3be25cfb1aaa
Nguyen Trung Khanh <nguyentrungkhanh97@gmail.com>
2020-09-04 11:21:52 +0700

    fix

--- filesys/cdfs/volinfo.c
+++ filesys/cdfs/volinfo.c
@@ -156,6 +156,8 @@ Return Value:
         //  and false if it couldn't wait for any I/O to complete.
         //
 
+        RtlZeroMemory(Irp->AssociatedIrp.SystemBuffer, Length);
+
         switch (IrpSp->Parameters.QueryVolume.FsInformationClass) {
 
         case FileFsSizeInformation:
