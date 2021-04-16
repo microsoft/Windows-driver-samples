5ab839435fd1c6d5f60171312cc0b078544b060d
Fredrik Orderud <forderud@users.noreply.github.com>
2021-04-17 01:39:34 +0200

    Remove duplicate VHIDMINI_DEVICE_STRING define (#579)
    The same define is also found 4 lines below.

--- hid/vhidmini2/inc/common.h
+++ hid/vhidmini2/inc/common.h
@@ -37,7 +37,6 @@ Environment:
 #define TEST_COLLECTION_REPORT_ID                         0x02
 
 #define MAXIMUM_STRING_LENGTH           (126 * sizeof(WCHAR))
-#define VHIDMINI_DEVICE_STRING          L"UMDF Virtual hidmini device"  
 #define VHIDMINI_MANUFACTURER_STRING    L"UMDF Virtual hidmini device Manufacturer string"  
 #define VHIDMINI_PRODUCT_STRING         L"UMDF Virtual hidmini device Product string"  
 #define VHIDMINI_SERIAL_NUMBER_STRING   L"UMDF Virtual hidmini device Serial Number string"  
