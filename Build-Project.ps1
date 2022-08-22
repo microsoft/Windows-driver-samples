9ad7e88bf7486b8b1027ba63f85e8dd4d33d4b81
Adonais Romero Gonzalez <seromero@microsoft.com>
2022-08-22 13:20:23 -0700

    Revert to windows-2019; add error message

--- Build-Project.ps1
+++ Build-Project.ps1
@@ -18,5 +18,6 @@ Write-Output "[$ProjectName] ⚒️ Building project..."
 msbuild $Directory -clp:Verbosity=m -t:clean,build -property:Configuration=$Configuration -property:Platform=$Platform -p:TargetVersion=Windows10 -p:InfVerif_AdditionalOptions="/msft /sw1205 /sw1324 /sw1420 /sw1421" -p:SignToolWS=/fdws -p:DriverCFlagAddOn=/wd4996 -flp1:errorsonly`;logfile=$errorLogFilePath -flp2:WarningsOnly`;logfile=$warnLogFilePath -noLogo
 if ($LASTEXITCODE -ne 0)
 {
+    Write-Error "Build for [$ProjectName] failed."
     Write-Output "❌ Build failed. Log available at $errorLogFilePath"
 }
