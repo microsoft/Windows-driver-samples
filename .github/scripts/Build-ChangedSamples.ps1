2be891a5bfb38558deead9b97e0432879564c0ff
Adonais Romero González <adonais.neoadonis@gmail.com>
2023-05-01 10:37:01 -0700

    Build all samples on PRs with changes to scripts/workflows

--- .github/scripts/Build-ChangedSamples.ps1
+++ .github/scripts/Build-ChangedSamples.ps1
@@ -10,31 +10,51 @@ if ($PSBoundParameters.ContainsKey('Verbose')) {
 
 $root = (Get-Location).Path
 
-# To include in CI gate
+# Search for samples (directories) to build
 $sampleSet = @{}
-foreach ($file in $ChangedFiles)
-{
+$buildAll = $false
+foreach ($file in $ChangedFiles) {
     if (-not (Test-Path $file)) {
         Write-Verbose "`u{2754} Changed file $file cannot be found"
         continue
     }
     $dir = (Get-Item $file).DirectoryName
-    while ((-not ($slnItems = (Get-ChildItem $dir '*.sln'))) -and ($dir -ne $root))
+    $filename = Split-Path $file -Leaf
+
+    # Files that can affect how every sample is built should trigger a full build
+    if ($filename -eq "Build-AllSamples.ps1" -or $filename -eq "Build-Sample.ps1") {
+        $buildAll = true
+    }
+    if ($dir -like "$root\.github\scripts*") {
+        $buildAll = true
+    }
+    if ($dir -like "$root\.github\workflows*") {
+        $buildAll = true
+    }
+    if ($buildAll)
     {
+        Write-Verbose "`u{2754} Full build triggered by change in file $file"
+        break
+    }
+
+    while ((-not ($slnItems = (Get-ChildItem $dir '*.sln'))) -and ($dir -ne $root)) {
         $dir = (Get-Item $dir).Parent.FullName
     }
-    if ($dir -eq $root)
-    {
+    if ($dir -eq $root) {
         Write-Verbose "`u{2754} Changed file $file does not match a sample."
         continue
     }
     $sampleName = $dir.Replace($root, '').Trim('\').Replace('\', '.').ToLower()
     Write-Verbose "`u{1F50E} Found sample [$sampleName] at $dir from changed file $file"
-    if (-not ($sampleSet.ContainsKey($sampleName)))
-    {
+    if (-not ($sampleSet.ContainsKey($sampleName))) {
         $sampleSet[$sampleName] = $dir
     }
 }
 
-.\Build-SampleSet -SampleSet $sampleSet -Verbose:$Verbose -LogFilesDirectory (Join-Path $root "_logs")
+if ($buildAll) {
+    .\Build-AllSamples -Verbose:$Verbose -LogFilesDirectory (Join-Path $root "_logs")
+}
+else {
+    .\Build-SampleSet -SampleSet $sampleSet -Verbose:$Verbose -LogFilesDirectory (Join-Path $root "_logs")
+}
 
