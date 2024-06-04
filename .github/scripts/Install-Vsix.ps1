d08f4d7eb640e390d6b05aa5b8ee5879f3ea6954
Matt <138825652+middlemose@users.noreply.github.com>
2024-06-04 11:13:54 -0700

    Changed CI pipeline to use nuget

---
+++ .github/scripts/Install-Vsix.ps1
@@ -0,0 +1,52 @@
+<#
+
+.SYNOPSIS
+Checks WDK vsix version and downloads and installs as necessary.
+
+#>
+
+$root = Get-Location
+
+# launch developer powershell (if necessary)
+if (-not $env:VSCMD_VER) {
+    Import-Module (Resolve-Path "$env:ProgramFiles\Microsoft Visual Studio\2022\*\Common7\Tools\Microsoft.VisualStudio.DevShell.dll")
+    Enter-VsDevShell -VsInstallPath (Resolve-Path "$env:ProgramFiles\Microsoft Visual Studio\2022\*")
+    cd $root
+}
+
+# source environment variables
+. .\.github\scripts\Env-Vars.ps1
+
+$version = $env:SAMPLES_VSIX_VERSION
+$uri = $env:SAMPLES_VSIX_URI
+
+function PrintWdkVsix {
+    $installed = ls "${env:ProgramData}\Microsoft\VisualStudio\Packages\Microsoft.Windows.DriverKit,version=*" | Select -ExpandProperty Name
+    "WDK Vsix Version: $isntalled"
+}
+
+function TestWdkVsix {
+    Test-Path "${env:ProgramData}\Microsoft\VisualStudio\Packages\Microsoft.Windows.DriverKit,version=$version"
+}
+
+PrintWdkVsix
+if (-not (TestWdkVsix)) {
+    "The correct WDK vsix is not installed."
+    "Will attempt to download and install now..."
+    Invoke-WebRequest -Uri "$uri" -OutFile wdk.vsix
+    "Finished downloading."
+    "Starting install process. This will take some time to complete..."
+    Start-Process vsixinstaller -ArgumentList "/f /q .\wdk.vsix" -wait
+    "The install process has finished."
+    "Checking the WDK.vsix version installed..."
+    if (TestWdkVsix) {
+        PrintWdkVsix
+        "The WDK vsix version is OK"
+    }
+    else {
+        "The WDK vsix install FAILED"
+        Write-Host "`u{274C} wdk vsix install had an issue"
+        Write-Error "the wdk vsix cannot be installed at this time"
+        exit 1
+    }
+}
--- Build-SampleSet.ps1
+++ Build-SampleSet.ps1
@@ -9,6 +9,17 @@ param(
 )
 
 $root = Get-Location
+
+# launch developer powershell (if necessary)
+if (-not $env:VSCMD_VER) {
+    Import-Module (Resolve-Path "$env:ProgramFiles\Microsoft Visual Studio\2022\*\Common7\Tools\Microsoft.VisualStudio.DevShell.dll")
+    Enter-VsDevShell -VsInstallPath (Resolve-Path "$env:ProgramFiles\Microsoft Visual Studio\2022\*")
+    cd $root
+}
+
+# source environment variables
+. .\.github\scripts\Env-Vars.ps1
+
 $ThrottleFactor = 5
 $LogicalProcessors = (Get-CIMInstance -Class 'CIM_Processor' -Verbose:$false).NumberOfLogicalProcessors
 
@@ -56,16 +67,16 @@ $build_number=0
 #
 # Hack: In GitHub we do not have an environment variable where we can see WDK build number, so we have it hard coded.
 #
-if (-not $env:GITHUB_REPOSITORY -eq '') {
+if ($env:GITHUB_REPOSITORY) {
     $build_environment="GitHub"
-    $build_number=22621
+    $build_number=$env:SAMPLES_BUILD_NUMBER
 }
 #
 # Hack: If user has hydrated nuget packages, then use those. That will be indicated by presence of a folder named .\packages.
 #
 elseif(Test-Path(".\packages")) {
     $build_environment=("NuGet")
-    $build_number=26100
+    $build_number=$env:SAMPLES_BUILD_NUMBER
 }
 #
 # EWDK sets environment variable BuildLab.  For example 'ni_release_svc_prod1.22621.2428'.
@@ -153,8 +164,12 @@ $jresult = @{
 
 $SolutionsTotal = $sampleSet.Count * $Configurations.Count * $Platforms.Count
 
+# set vsix version
+$wdk_vsix_version = ls "${env:ProgramData}\Microsoft\VisualStudio\Packages\Microsoft.Windows.DriverKit,version=*" | Select -ExpandProperty Name
+
 Write-Output ("Build Environment:          " + $build_environment)
 Write-Output ("Build Number:               " + $build_number)
+Write-Output ("WDK VSIX Version:           " + $wdk_vsix_version)
 Write-Output ("Samples:                    " + $sampleSet.Count)
 Write-Output ("Configurations:             " + $Configurations.Count + " (" + $Configurations + ")")
 Write-Output ("Platforms:                  " + $Platforms.Count + " (" + $Platforms + ")")
